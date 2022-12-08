681c24da5f2acab26fd113157de4a31d9a8c924f
nanidileepy <nanidileepy@gmail.com>
2022-12-08 16:11:18 +0530

    update lines in

--- NoXmlFromSpring/src/main/java/com/nxml/beans/App.java
+++ NoXmlFromSpring/src/main/java/com/nxml/beans/App.java
@@ -10,5 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hi");
+         System.out.println( "Hello World!" );
+        System.out.println("hi");
+         System.out.println( "Hello World!" );
+        System.out.println("hi");
     }
 }
