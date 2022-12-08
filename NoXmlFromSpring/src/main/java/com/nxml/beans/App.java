bb158eb6787019b915a8f9be7ee63c6ff66bf9ac
nanidileepy <nanidileepy@gmail.com>
2022-12-08 14:53:09 +0530

    agin

--- NoXmlFromSpring/src/main/java/com/nxml/beans/App.java
+++ NoXmlFromSpring/src/main/java/com/nxml/beans/App.java
@@ -4,11 +4,14 @@ package com.nxml.beans;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("hi");
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("hi");
+		System.out.println("Hello World!");
+		System.out.println("hi");
+		System.out.println("Hello World!");
+		System.out.println("hi");
+
+	}
 }
